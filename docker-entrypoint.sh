be7fc8a821c0cc46d5953108bc88f69c51a604e6
Zadkiel Aharonian <hello@zadkiel.fr>
2023-10-05 15:48:34 +0200

    feat: add proxy protocol option (#11)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,16 +4,31 @@ set -e
 
 BIND_ADDRESS=${BIND_ADDRESS:-0.0.0.0}
 BIND_PORT=${BIND_PORT:-8443}
+PROXY_PROTOCOL=${PROXY_PROTOCOL:-0}
 
 [ -z "$TARGET_HOST" ] && echo "TARGET_HOST is required" && exit 1
 [ -z "$TARGET_PORT" ] && echo "TARGET_PORT is required" && exit 1
 
-sed \
+cp /usr/local/etc/envoy/config.yml.tmpl /usr/local/etc/envoy/config.yml
+
+sed -i \
   -e "s/__TMPL_BIND_ADDRESS__/$BIND_ADDRESS/g" \
   -e "s/__TMPL_BIND_PORT__/$BIND_PORT/g" \
   -e "s/__TMPL_TARGET_HOST__/$TARGET_HOST/g" \
   -e "s/__TMPL_TARGET_PORT__/$TARGET_PORT/g" \
-  /usr/local/etc/envoy/config.yml.tmpl | \
-  tee /usr/local/etc/envoy/config.yml
+  /usr/local/etc/envoy/config.yml
+
+if [ "$PROXY_PROTOCOL" = 0 ]; then
+  sed -i \
+    -e "/__TMPL_BEGIN_PROXY_PROTOCOL__/,/__TMPL_END_PROXY_PROTOCOL__/d" \
+    /usr/local/etc/envoy/config.yml
+else
+  sed -i \
+    -e "/__TMPL_BEGIN_PROXY_PROTOCOL__/d" \
+    -e "/__TMPL_END_PROXY_PROTOCOL__/d" \
+    /usr/local/etc/envoy/config.yml
+fi
+
+cat /usr/local/etc/envoy/config.yml
 
 exec envoy -c /usr/local/etc/envoy/config.yml "$@"
