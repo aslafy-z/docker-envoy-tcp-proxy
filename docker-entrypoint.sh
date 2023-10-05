9589a7e6afcdd980620b3eb111783b3aa085bb0f
Zadkiel Aharonian <hello@zadkiel.fr>
2023-10-05 15:01:18 +0200

    feat: add tcp proxy option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 BIND_ADDRESS=${BIND_ADDRESS:-0.0.0.0}
 BIND_PORT=${BIND_PORT:-8443}
+TCP_PROXY=${BIND_PORT:-8443}
 
 [ -z "$TARGET_HOST" ] && echo "TARGET_HOST is required" && exit 1
 [ -z "$TARGET_PORT" ] && echo "TARGET_PORT is required" && exit 1
@@ -13,7 +14,11 @@ sed \
   -e "s/__TMPL_BIND_PORT__/$BIND_PORT/g" \
   -e "s/__TMPL_TARGET_HOST__/$TARGET_HOST/g" \
   -e "s/__TMPL_TARGET_PORT__/$TARGET_PORT/g" \
-  /usr/local/etc/envoy/config.yml.tmpl | \
+  /usr/local/etc/envoy/config.yml.tmpl
+
+sed '/__TMPL_BEGIN_TCP_PROXY__/,/__TMPL_END_TCP_PROXY__/d' envoy.tpl
+
+| \
   tee /usr/local/etc/envoy/config.yml
 
 exec envoy -c /usr/local/etc/envoy/config.yml "$@"
