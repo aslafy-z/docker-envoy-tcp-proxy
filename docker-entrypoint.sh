4cd7116fce0e0bfff278b396071de0145cc0020a
Zadkiel Aharonian <hello@zadkiel.fr>
2023-10-05 15:17:14 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,21 +4,23 @@ set -e
 
 BIND_ADDRESS=${BIND_ADDRESS:-0.0.0.0}
 BIND_PORT=${BIND_PORT:-8443}
-TCP_PROXY=${BIND_PORT:-8443}
+PROXY_PROTOCOL=${PROXY_PROTOCOL:-0}
 
 [ -z "$TARGET_HOST" ] && echo "TARGET_HOST is required" && exit 1
 [ -z "$TARGET_PORT" ] && echo "TARGET_PORT is required" && exit 1
 
-sed \
+sed -i \
   -e "s/__TMPL_BIND_ADDRESS__/$BIND_ADDRESS/g" \
   -e "s/__TMPL_BIND_PORT__/$BIND_PORT/g" \
   -e "s/__TMPL_TARGET_HOST__/$TARGET_HOST/g" \
   -e "s/__TMPL_TARGET_PORT__/$TARGET_PORT/g" \
   /usr/local/etc/envoy/config.yml.tmpl
 
-sed '/__TMPL_BEGIN_TCP_PROXY__/,/__TMPL_END_TCP_PROXY__/d' envoy.tpl
+[ "$PROXY_PROTOCOL" = 1 ] && \
+  sed -i \
+  '/__TMPL_BEGIN_PROXY_PROTOCOL__/,/__TMPL_END_PROXY_PROTOCOL__/d' \
+  /usr/local/etc/envoy/config.yml.tmpl
 
-| \
-  tee /usr/local/etc/envoy/config.yml
+cat /usr/local/etc/envoy/config.yml
 
 exec envoy -c /usr/local/etc/envoy/config.yml "$@"
