2bc50f58e7e6b04e6e9cdb4885c69116b84bb796
Zadkiel Aharonian <hello@zadkiel.fr>
2023-10-05 15:20:16 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,13 +18,13 @@ sed -i \
 
 if [ "$PROXY_PROTOCOL" = 1 ]; then
   sed -i \
-  '/__TMPL_BEGIN_PROXY_PROTOCOL__/,/__TMPL_END_PROXY_PROTOCOL__/d' \
-  /usr/local/etc/envoy/config.yml.tmpl
+    -e '/__TMPL_BEGIN_PROXY_PROTOCOL__/,/__TMPL_END_PROXY_PROTOCOL__/d' \
+    /usr/local/etc/envoy/config.yml.tmpl
 else
   sed -i \
-  -e 's/__TMPL_BEGIN_PROXY_PROTOCOL__//' \
-  -e 's/__TMPL_END_PROXY_PROTOCOL__//' \
-  /usr/local/etc/envoy/config.yml.tmpl
+    -e 's/__TMPL_BEGIN_PROXY_PROTOCOL__//' \
+    -e 's/__TMPL_END_PROXY_PROTOCOL__//' \
+    /usr/local/etc/envoy/config.yml.tmpl
 fi
 
 cat /usr/local/etc/envoy/config.yml
