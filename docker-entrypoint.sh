78a7afb69bd91777c7e18ed1687163ccaeba599a
Zadkiel Aharonian <hello@zadkiel.fr>
2024-04-10 11:45:29 +0200

    feat: add admin endpoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 BIND_ADDRESS=${BIND_ADDRESS:-0.0.0.0}
 BIND_PORT=${BIND_PORT:-8443}
 PROXY_PROTOCOL=${PROXY_PROTOCOL:-0}
+ADMIN_ENDPOINT=${ADMIN_ENDPOINT:-0}
+ADMIN_ADDRESS=${ADMIN_ADDRESS:-0.0.0.0}
+ADMIN_PORT=${BIND_PORT:-9901}
 
 [ -z "$TARGET_HOST" ] && echo "TARGET_HOST is required" && exit 1
 [ -z "$TARGET_PORT" ] && echo "TARGET_PORT is required" && exit 1
@@ -26,6 +29,19 @@ else
   sed -i \
     -e "/__TMPL_BEGIN_PROXY_PROTOCOL__/d" \
     -e "/__TMPL_END_PROXY_PROTOCOL__/d" \
+    /usr/local/etc/envoy/config.yml
+fi
+
+if [ "$ADMIN_ENDPOINT" = 0 ]; then
+  sed -i \
+    -e "/__TMPL_BEGIN_ADMIN_ENDPOINT__/,/__TMPL_END_ADMIN_ENDPOINT__/d" \
+    /usr/local/etc/envoy/config.yml
+else
+  sed -i \
+    -e "/__TMPL_BEGIN_ADMIN_ENDPOINT__/d" \
+    -e "/__TMPL_END_ADMIN_ENDPOINT__/d" \
+    -e "s/__TMPL_ADMIN_ADDRESS__/$ADMIN_ADDRESS/g" \
+    -e "s/__TMPL_ADMIN_PORT__/$ADMIN_PORT/g" \
     /usr/local/etc/envoy/config.yml
 fi
 
